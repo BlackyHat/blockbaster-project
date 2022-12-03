b2411fb2609e6abdb3a1f6ac4dd703ff9ee09e5d
Oleksandr P <goodwin009@gmail.com>
2022-12-03 13:47:22 +0200

    initial commit

--- src/js/films.js
+++ src/js/films.js
@@ -2,3 +2,4 @@ import { FilmsApiService } from './search-api';
 
 const filmsApiService = new FilmsApiService();
 // на вкладці network вже є дані котрі можна рендерити та додавати до search api метод роботи з пошукоими запитами
+console.log(filmsApiService.getTrendingDataApi());
