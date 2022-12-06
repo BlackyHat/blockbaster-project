ee38468e2e9ef16baad1364a011523f8965240a7
BlackyHat <105644022+BlackyHat@users.noreply.github.com>
2022-12-06 14:51:42 +0000

    Deploying to gh-pages from @ BlackyHat/blockbaster-project@4f0fd4944ade0cb7660bf1cc1377f2bc9fc7c522 🚀

---
+++ index.b1f68ba8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function t(){e.modal.classList.toggle("is-hidden")}document.addEventListener("click",(function(t){t.target.id===e.openModalBtn.id&&t.preventDefault()})),e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.b1f68ba8.js.map
