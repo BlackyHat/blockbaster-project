18ea460af1a9df52cd8dfc2c0ff5524abf081e49
BlackyHat <105644022+BlackyHat@users.noreply.github.com>
2022-12-07 07:09:59 +0000

    Deploying to gh-pages from @ BlackyHat/blockbaster-project@7d0cd9eab9ae80eb79540a02c300109e2c2ef9de 🚀

---
+++ index.081ae09a.js
@@ -0,0 +1,2 @@
+var svg=document.querySelector(".footer__icon");function blickSvg(){setInterval((function(){svg.classList.toggle("footer__icon-blick")}),800)}blickSvg();
+//# sourceMappingURL=index.081ae09a.js.map
