6c3f8cac0cda2b2c86604889f2e6e69c276c5829
Oleksandr P <goodwin009@gmail.com>
2022-12-09 13:12:05 +0200

    fix open/close modal

--- src/js/modal-students.js
+++ src/js/modal-students.js
@@ -1,24 +1,39 @@
-import SimpleBar from 'simplebar';
-import 'simplebar/dist/simplebar.css';
+// import SimpleBar from 'simplebar';
+// import 'simplebar/dist/simplebar.css';
 
-new SimpleBar(document.getElementById('bar'));
-
+// new SimpleBar(document.getElementById('bar'));
 const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  backdrop: document.querySelector('.backdrop'),
 };
 
-
-document.addEventListener('click', (e) => {
-    if (e.target.id === refs.openModalBtn.id) {
-        e.preventDefault();
-    }
-});
-  
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+document.addEventListener('click', e => {
+  if (e.target.id === refs.openModalBtn.id) {
+    e.preventDefault();
   }
+});
+console.log(refs.closeModalBtn);
+
+refs.openModalBtn.addEventListener('click', openModal);
+
+function openModal() {
+  refs.modal.classList.remove('is-hidden');
+  window.addEventListener('keydown', onEscPress);
+  refs.closeModalBtn.addEventListener('click', closeModal);
+  refs.backdrop.addEventListener('click', e => {
+    if (e.target.classList.contains('backdrop')) {
+      closeModal();
+    }
+  });
+}
+function closeModal() {
+  refs.modal.classList.add('is-hidden');
+}
+function onEscPress(e) {
+  if (e.code === 'Escape') {
+    closeModal();
+    window.removeEventListener('keydown', onEscPress);
+  }
+}
