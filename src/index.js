f8aba5c0bd8d6f027c1f63a3bdba7cf1d5eb7842
Oleksandr P <goodwin009@gmail.com>
2022-12-30 10:25:29 +0200

    add button down/up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { getTrendMovies } from './js/films.js';
 import './js/genres-bar';
 import './js/modal-movie.js';
+import { scrollUp } from './js/scrollToTop.js';
 import './js/blinking-heart.js';
 import './js/modal-students.js';
 
 getTrendMovies();
+scrollUp();
+window.addEventListener('scroll', scrollUp);
